03279538cba8fb577fc50c5c96c691ae11b5822f
William Chen <76022647+bobbybotbop@users.noreply.github.com>
2025-04-06 18:40:17 -0400

    added routes, / and /favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import MovieCard from "./Components/MovieCard";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      {
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      }
+    </main>
   );
 }
 
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,8 @@
-function Favorite() {
+function Favorites() {
   return (
     <div className="favorites-empty">
       <h2>no favorite movies yet</h2>
     </div>
   );
 }
-export default Favorite;
+export default Favorites;
